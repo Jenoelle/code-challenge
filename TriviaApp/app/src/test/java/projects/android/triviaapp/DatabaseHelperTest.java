9195f2e8f7fdb7fc50172a97a5b6e5c376972409
JessisUnreal <jjtaytay33@yahoo.com>
2019-07-26 16:51:03 -0700

    Database fixes

---
+++ TriviaApp/app/src/test/java/projects/android/triviaapp/DatabaseHelperTest.java
@@ -0,0 +1,36 @@
+package projects.android.triviaapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class DatabaseHelperTest {
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void addQuestion() {
+    }
+
+    @Test
+    public void getAllQuestions() {
+    }
+
+    @Test
+    public void getQuestionsOfCategory() {
+    }
+
+    @Test
+    public void getQuestionsOfDifficulty() {
+    }
+
+    @Test
+    public void getQuestionsCount() {
+    }
+
+    @Test
+    public void deleteAllQuestions() {
+    }
+}
